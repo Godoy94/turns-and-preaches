b242f7cfa549b654c97c8633a714c066fd1e5382
Gustavo Godoy Domingues <gustavogodoyd@brq.com>
2022-01-29 04:52:34 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,82 @@
+let imagens = new Array();
+imagens[0] = "<h2>Abajur</h2> <img src='./img/abajur.jpg' >";
+imagens[1] = "<h2>Acredite</h2> <img src='./img/acredite.jpg '>";
+imagens[2] = "<h2>Ajudar</h2> <img src='./img/ajudar.jpg '>";
+imagens[3] = "<h2>Alegria</h2> <img src='./img/alegria.jpg '>";
+imagens[4] = "<h2>Alimento</h2> <img src='./img/alimento.jpg '>";
+imagens[5] = "<h2>Almofada</h2> <img src='./img/almofada.jpg '>";
+imagens[6] = "<h2>Amizade</h2> <img src='./img/amizade.jpg '>";
+imagens[7] = "<h2>Aniversário</h2> <img src='./img/aniversário.webp '>";
+imagens[8] = "<h2>Babilonia</h2> <img src='./img/babilonia.jpg '>";
+imagens[9] = "<h2>Baheeea</h2> <img src='./img/baheeea.jpg '>";
+imagens[10] = "<h2>Bíblia</h2> <img src='./img/biblia.jpg '>";
+imagens[11] = "<h2>Big Mc</h2> <img src='./img/big-mc.jpg '>";
+imagens[12] = "<h2>Biriba</h2> <img src='./img/biriba.jpg '>";
+imagens[13] = "<h2>Bola</h2> <img src='./img/bola.png '>";
+imagens[14] = "<h2>Caminho</h2> <img src='./img/caminho.jpg '>"
+imagens[15] = "<h2>Carro</h2> <img src='./img/carro.jpg '>";
+imagens[16] = "<h2>Celula</h2> <img src='./img/celula.jpg '>";
+imagens[17] = "<h2>Chinelo</h2> <img src='./img/chinelo.jpg '>"
+imagens[18] = "<h2>Chuva</h2> <img src='./img/chuva.jpg '>";
+imagens[19] = "<h2>Cidade</h2> <img src='./img/cidade.jpg '>";
+imagens[20] = "<h2>Comunhão</h2> <img src='./img/comunhão.jpg '>";
+imagens[21] = "<h2>Controle-ps4</h2> <img src='./img/controle-ps4.jpg '>"
+imagens[22] = "<h2>Coração Quebrado</h2> <img src='./img/coração-quebrado.png '>"
+imagens[23] = "<h2>Davi & Golias</h2> <img src='./img/davi-golias.jpg '>";
+imagens[24] = "<h2>Isfirri</h2> <img src='./img/esfirra.png '>";
+imagens[25] = "<h2>Estratégia</h2> <img src='./img/estrategia.jpg '>";
+imagens[26] = "<h2>Estrutura</h2> <img src='./img/estrutura.jpg '>";
+imagens[27] = "<h2>Fé</h2> <img src='./img/fé.jpg '>";
+imagens[28] = "<h2>Futebol</h2> <img src='./img/futebol.jpg '>";
+imagens[29] = "<h2>Granada</h2> <img src='./img/granada.jpg '>";
+imagens[30] = "<h2>Gratidão</h2> <img src='./img/gratidao.webp '>";
+imagens[31] = "<h2>Guarda-chuva</h2> <img src='./img/guarda-chuva.jpg '>";
+imagens[32] = "<h2>Guarda-roupa</h2> <img src='./img/guarda-roupa.jpg '>";
+imagens[33] = "<h2>Hora</h2> <img src='./img/hora.jpg '>";
+imagens[34] = "<h2>Igreja videira</h2> <img src='./img/igreja-videira.jpg '>";
+imagens[35] = "<h2>Irmãos</h2> <img src='./img/irmaos.jpg '>";
+imagens[36] = "<h2>Jejum</h2> <img src='./img/jejum.jpg '>";
+imagens[37] = "<h2>Jogo-vida</h2> <img src='./img/jogo-vida.jpg '>";
+imagens[38] = "<h2>Leao</h2> <img src='./img/leao.jpg '>";
+imagens[39] = "<h2>Liberdade</h2> <img src='./img/liberdade.jpg '>";
+imagens[40] = "<h2>Ligação</h2> <img src='./img/ligaçao.jpg '>";
+imagens[41] = "<h2>Localização</h2> <img src='./img/localização.jpeg '>";
+imagens[42] = "<h2>Louco</h2> <img src='./img/louco.jpg '>";
+imagens[43] = "<h2>Luz</h2> <img src='./img/luz.jpg '>";
+imagens[44] = "<h2>Makita</h2> <img src='./img/makita.jpg '>";
+imagens[45] = "<h2>Marea</h2> <img src='./img/marea.jpg '>";
+imagens[46] = "<h2>Marginal Pinheiros</h2> <img src='./img/marginal-pinheiros.jpg '>";
+imagens[47] = "<h2>Marginal Tiete</h2> <img src='./img/marginal-tiete.jpg '>";
+imagens[48] = "<h2>Moises</h2> <img src='./img/moises.jpg '>";
+imagens[49] = "<h2>Moto</h2> <img src='./img/moto.jpg '>";
+imagens[50] = "<h2>Multidão</h2> <img src='./img/multidão.jpg '>";
+imagens[51] = "<h2>Música</h2> <img src='./img/musica.png '>";
+imagens[52] = "<h2>Ovelha</h2> <img src='./img/ovelha.jpg '>";
+imagens[53] = "<h2>Paciência</h2> <img src='./img/paciencia.jpg '>";
+imagens[54] = "<h2>Paz</h2> <img src='./img/paz.jpg '>";
+imagens[55] = "<h2>Pincel</h2> <img src='./img/pincel.jpg '>";
+imagens[56] = "<h2>Planeta</h2> <img src='./img/planeta.jpg '>";
+imagens[57] = "<h2>Pneu</h2> <img src='./img/pneu.jpg '>";
+imagens[58] = "<h2>Praia</h2> <img src='./img/praia.jpg '>";
+imagens[59] = "<h2>Prisão</h2> <img src='./img/prisao.jpg '>";
+imagens[60] = "<h2>Ressureição</h2> <img src='./img/ressureicao.jpg '>";
+imagens[61] = "<h2>Sonho</h2> <img src='./img/sonho.jpg '>";
+imagens[62] = "<h2>Sono</h2> <img src='./img/sono.jpg '>";
+imagens[63] = "<h2>Tesoura</h2> <img src='./img/tesoura.jpg '>";
+imagens[64] = "<h2>Tijolo</h2> <img src='./img/tijolo.jpg '>";
+imagens[65] = "<h2>Tinta</h2> <img src='./img/tinta.jpg '>";
+imagens[66] = "<h2>Tristeza</h2> <img src='./img/tristeza.jpg '>";
+imagens[67] = "<h2>Vergonha</h2> <img src='./img/vergonha.png '>";
+
+
+let aleatory = Math.floor(Math.random() * imagens.length);
+console.log(aleatory);
+var escreve = document.querySelector('#resultado').innerHTML = imagens[aleatory];
+
+    const reloadPage = () => {
+        document.location.reload(true);
+        console.log("Mudei a pagina");
+    }
+
+
+setInterval(reloadPage, 5000);
